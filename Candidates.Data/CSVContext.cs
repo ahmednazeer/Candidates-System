150d91f7d8e5ad1aba1b5fb6b3d30245039e0229
Ahmed_Nazeer <ahmedgelany45@gmail.com>
2022-11-05 22:46:57 +0200

    add context logic

---
+++ Candidates.Data/CSVContext.cs
@@ -0,0 +1,36 @@
+﻿using Candidates.Data.contracts;
+using Candidates.Helpers;
+using CsvHelper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Candidates.Data
+{
+    public class CSVContext : IDataContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public CSVContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        
+        public void SaveChanges()
+        {
+            
+            {
+                using (var writer = new StreamWriter(_configuration.GetSection("csvFileLocation").Value))
+                using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    var records = CandidatesDataReader.CandidatesDict.Values.ToList();
+                    csv.WriteRecords(records);
+                }
+            }
+        }
+    }
+}
---
+++ Candidates.Data/contracts/IDataContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Candidates.Data.contracts
+{
+    public interface IDataContext
+    {
+        void SaveChanges();
+    }
+}
